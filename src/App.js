81487d0ad87678e0d9a1e825e9e80be564f0dbf2
Bayram Alkan <bayramalkan@ogrenci.beykoz.edu.tr>
2021-11-02 16:49:59 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [loading, setLoading] = useState(null)
+  const [coords, setCoords] = useState(null)
+  const [current, setCurrent] =useState({
+    temp_c:'',
+    icon:'',
+    feelslike_c:'',
+    
+  })
+
+  const [location, setLocation] =useState({
+    name:'',
+    region:'',
+    country:'',
+    localtime:'',
+  })
+
+  const getLocation = () =>{
+    if(!navigator.geolocation){
+      alert("Konum bilginizi açmalısınız!")
+      
+    }else{
+      setLoading(true)
+      
+      navigator.geolocation.getCurrentPosition(position=>{
+        
+        setCoords(`${position.coords.latitude},${position.coords.longitude}`)
+      
+      })
+    }
+  }
+
+  useEffect(()=>{
+    setLoading(true)
+    const key = process.env.REACT_APP_WEATHER_API_KEY;
+    const apiURL = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${coords}&aqi=no
+    `
+    fetch(apiURL).then(res=>res.json()).then((data)=>{
+      //console.log(data);
+      setCurrent({
+        temp:data.current.temp_c,
+        icon:data.current.condition.icon,
+        feelslike_c:data.current.feelslike_c
+      })
+      setLocation({
+        name:data.location.name,
+        region:data.location.region,
+        country:data.location.country,
+        localtime:data.location.localtime,
+      })
+      setLoading(false)
+    })
+  },[coords])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <span>{coords}</span>
+        <button className="button" onClick={getLocation}>Hava Durumunu Getir</button>
+       
+        {!loading && <div>
+        
+        
+        <div className="weather">
+          <img src={current.icon} alt="weather" />
+          <div>
+            Sıcaklık
+          <span>{current.temp}</span>
+          </div>
+          <div>
+            Hissedilen
+          <span>{current.feelslike_c}</span>
+          </div>
+        </div>
+        <div>
+          <div className="location">
+            <span>{location.name}</span> - <span>{location.region}</span> - <span>{location.country}</span>
+          </div>
+          <div ><span className="time">{location.localtime}</span></div>
+        </div>
+        </div>
+        }
+        {loading && <div className="loading"><span>Yükleniyor</span> </div>}
+       
     </div>
   );
 }
